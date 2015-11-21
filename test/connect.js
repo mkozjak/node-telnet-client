f9ac7e2a83bd1b1c36fd6b1886e3594bedcc98cb
Mario Kozjak <kozjakm1@gmail.com>
2015-11-21 15:57:55 +0100

    test: connect: minor syntax fixes
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- test/connect.js
+++ test/connect.js
@@ -1,38 +1,38 @@
 var telnet = process.env.NODETELNETCLIENT_COV 
   ? require('../lib-cov/telnet-client')
-  : require('../lib/telnet-client');
-var nodeunit = require('nodeunit');
-var net = require('net');
+  : require('../lib/telnet-client')
+var nodeunit = require('nodeunit')
+var net = require('net')
 
-var socket, server, callbackCount;
+var socket, server, callbackCount
 
 exports['socket'] = nodeunit.testCase({
   setUp: function(callback) {
-    socket = new telnet();
-    callbackCount = 0;
+    socket = new telnet()
+    callbackCount = 0
     server = net.createServer(function(c) {
-      callbackCount++;
-      c.end();
+      callbackCount++
+      c.end()
     })
     server.listen(2323, function(err) {
-      callback();
-    });
+      callback()
+    })
   },
 
   tearDown: function(callback) {
     server.close(function() {
-      callback();
-    });
+      callback()
+    })
   },
 
-  "connect": function(test) {
+  connect: function(test) {
     socket.connect({
       host: '127.0.0.1',
       port: 2323 //not using 23 is a service port could need sudo 
-    });
+    })
     socket.on('connect', function() {
-      test.ok(callbackCount == 1, "Client did connect");
-      test.done();
-    });
+      test.ok(callbackCount == 1, "Client did connect")
+      test.done()
+    })
   }
-});
+})
