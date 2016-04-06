8bd2a08c5b75c2c7eb073fdafd3983fb678e7475
Mario Kozjak <kozjakm1@gmail.com>
2016-04-06 22:49:38 +0200

    test: server push
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

---
+++ test/003_server_push.js
@@ -0,0 +1,50 @@
+var telnet = process.env.NODETELNETCLIENT_COV 
+  ? require('../lib-cov/telnet-client')
+  : require('../lib/telnet-client')
+var nodeunit = require('nodeunit')
+var telnet_server = require('telnet')
+
+var srv
+
+exports['busybox'] = nodeunit.testCase({
+  setUp: function(callback) {
+    srv = telnet_server.createServer(function(c) {
+      c.write(new Buffer("BusyBox v1.19.2 () built-in shell (ash)\n"
+        + "Enter 'help' for a list of built-in commands.\n\n/ # ", 'ascii'))
+
+      setTimeout(function() {
+        c.write(new Buffer("Hello, client!", 'ascii'))
+      }, 50)
+    })
+    
+    srv.listen(2323, function() {
+      callback()
+    })
+  },
+
+  tearDown: function(callback) {
+    srv.close(function() {
+      callback()
+    })
+  },
+
+  receive_pushed_data: function(test) {
+    var connection = new telnet()
+
+    var params = {
+      host: '127.0.0.1',
+      port: 2323,
+      shellPrompt: '/ # ',
+      timeout: 1500
+    }
+
+    connection.on('data', function(data) {
+      connection.end()
+
+      test.strictEqual(data.toString(), 'Hello, client!')
+      test.done()
+    })
+    
+    connection.connect(params);
+  }
+})
