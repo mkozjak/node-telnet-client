702796945dfb8eb78f6d76964c19960abb5198f0
Mario Kozjak <kozjakm1@gmail.com>
2017-09-27 13:42:03 +0200

    don't set global prompt if regex provided; resolves #91
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

---
+++ test/011_dynamic_shellprompt.js
@@ -0,0 +1,54 @@
+var telnet = process.env.NODETELNETCLIENT_COV 
+  ? require('../lib-cov/index')
+  : require('../lib/index')
+var nodeunit = require('nodeunit')
+var telnet_server = require('telnet')
+
+var srv
+
+exports['dynamic_shellprompt'] = nodeunit.testCase({
+  setUp: function(callback) {
+    srv = telnet_server.createServer(function(c) {
+      c.write(new Buffer("BusyBox v1.19.2 () built-in shell (ash)\n"
+        + "Enter 'help' for a list of built-in commands.\n\n<prompt1>", 'ascii'))
+
+      c.on('data', function() {
+        c.write(new Buffer("uptime\r\n23:14  up 1 day, 21:50, 6 users, "
+          + "load averages: 1.41 1.43 1.41\r\n", 'ascii'))
+        c.write(new Buffer("[prompt2]", 'ascii'))
+      })
+    })
+    
+    srv.listen(2323, function() {
+      callback()
+    })
+  },
+
+  tearDown: function(callback) {
+    srv.close(function() {
+      callback()
+    })
+  },
+
+  dynamic_prompt_with_regex: function(test) {
+    var connection = new telnet()
+
+    var params = {
+      host: '127.0.0.1',
+      port: 2323,
+      shellPrompt: /(>|])/,
+      timeout: 1500
+    }
+
+    connection.on('ready', function(prompt) {
+      connection.exec('uptime', function(err, resp) {
+        connection.end()
+
+        test.strictEqual(resp, '23:14  up 1 day, 21:50, 6 users, load averages: 1.41 1.43 1.41\n')
+        test.done()
+      })
+    })
+    
+    connection.connect(params)
+  },
+})
