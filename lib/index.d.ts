ec7ccc19235993e95b513ad2eafdf733c3bcc6d3
Kyle Hensel <k-yle@users.noreply.github.com>
2019-10-02 20:18:40 +1300

    add typescript types (#145)

---
+++ lib/index.d.ts
@@ -0,0 +1,67 @@
+import { EventEmitter } from 'events';
+import { Stream } from 'stream';
+import { Socket } from 'net';
+
+declare interface ConnectOptions {
+    host?: string;
+    port?: number;
+    timeout?: number;
+    shellPrompt?: string;
+    loginPrompt?: string|RegExp;
+    passwordPrompt?: string|RegExp;
+    failedLoginMatch?: string|RegExp;
+    initialLFCR?: boolean;
+    username?: string;
+    password?: string;
+    sock?: Socket;
+    irs?: string;
+    ors?: string;
+    echoLines?: number;
+    pageSeparator?: string|RegExp;
+    negotiationMandatory?: boolean;
+    execTimeout: number;
+    sendTimeout: number;
+    sendTmaxBufferLengthimeout: number;
+    debug: boolean;
+}
+
+declare interface ExecOptions {
+    shellPrompt?: string;
+    loginPrompt?: string;
+    failedLoginMatch?: string;
+    timeout?: number;
+    execTimeout?: number;
+    irs?: string;
+    ors?: string;
+    echoLines?: number;
+    maxBufferLength?: number;
+}
+
+declare interface SendOptions {
+    ors?: string;
+    waitfor?: string|RegExp;
+    timeout?: number;
+    maxBufferLength?: number;
+}
+
+export default class telnet_client extends EventEmitter implements ConnectOptions {
+    constructor();
+
+    connect(params: ConnectOptions): Promise<void>;
+
+    destroy(): Promise<void>;
+
+    end(): Promise<void>;
+
+    exec(cmd: string, options?: ExecOptions): Promise<string>;
+
+    getSocket(): Socket;
+
+    send(cmd: string, options?: SendOptions): Promise<string>;
+
+    shell(): Promise<Stream>;
+
+    public socket: Socket;
+
+    public state: 'ready'|'start'|'standby'|'response'|'getprompt'|'login'|'failedlogin'|null;
+}
